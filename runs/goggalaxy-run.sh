60a9559cfe3224f66b6be472c3368d20b5abed26
Felipe Facundes <felipe.facundes@gmail.com>
2020-01-20 23:50:22 -0300

    Update goggalaxy-run.sh

--- runs/goggalaxy-run.sh
+++ runs/goggalaxy-run.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
+rm -rf ~/.local/share/applications/*wine*
 # PlayOnGit - Inicie seus Jogos direto do menu iniciar, sem precisar de PlayOnLinux, Proton ou Lutris, e com um desempenho muito melhor e superior.
 # Licença: GPLv3
 # Mantenedor: Felipe Facundes
 # Faça o seu pedido de tutorial e GamePlay no nosso:
 # 既 Grupo 調 Gamer do 切 Telegram 切: https://t.me/winehq_linux
 ########### Este script irá usar o wine personalizado. Mas, você poderá usar um wine na versão e local de sua escolha
-WV=wine-staging-4.11-1-x86_64
+WV=wine-staging-4.21-1-x86_64
 GN=goggalaxy
 SN="GOG Galaxy"
 CME="GOG Store"
 
-#1# Essa é a versão escolhida do Wine
+#A# Essa é a versão escolhida do Wine
 export TERM=xterm
-# Para ver o FPS na tela, para CPU, inclua cpu,fps
-#export GALLIUM_HUD="fps"
+# Para ver o FPS na tela e o uso da CPU, inclua cpu,fps
+#export GALLIUM_HUD="simple,fps"
 W=~/.jogos/wines/"$WV"
 export WINE64="$W"/bin/wine64
 export WINE="$W"/bin/wine
 export WINEVERPATH="$W"
-export WINEPATH="$W"/bin:$WINEPATH
+export WINEPATH="$W/bin:$WINEPATH"
 export WINESERVER="$W"/bin/wineserver
 export WINELOADER64="$W"/bin/wine64
 export WINELOADER="$W"/bin/wine
@@ -30,6 +31,7 @@ export LD_LIBRARY64_PATH="$W/lib:$LD_LIBRARY64_PATH"
 export LD_LIBRARY_PATH="$W/lib:$LD_LIBRARY_PATH"
 #"$W"/bin/wineconsole "cmd"
 
+#export WINEDEBUG=-all,fps
 export WINEDEBUG=-all
 # Prefix do wine, destino do prefix individual para cada jogo é melhor e evita futuras falhas
 export WINEPREFIX=~/.jogos/wineprefixes/"$GN"
@@ -37,13 +39,13 @@ export WINEPREFIX=~/.jogos/wineprefixes/"$GN"
 export WINEARCH=win64
 export WINEESYNC=0
 #export ESYNC=0
-export vblank_mode=0
 # Esta é uma opção que às vezes é necessária para alguns jogos   MESA_GL_VERSION_OVERRIDE=version
-export MESA_GLSL_VERSION_OVERRIDE=450
-export MESA_GL_VERSION_OVERRIDE=4.5COMPAT
+#export MESA_GLSL_VERSION_OVERRIDE=450
+#export MESA_GL_VERSION_OVERRIDE=4.5COMPAT
 # Para placas gráficas híbridas use o DRI_PRIME=1
 #export DRI_PRIME=1
-export WINEDLLOVERRIDES=d3d10,d3d11,dxgi=n
+#Origin.exe,OriginClientService.exe=d
+#export WINEDLLOVERRIDES=d3d10,d3d11,dxgi=n
 export DXVK_SPIRV_OPT=ON
 export DXVK_SHADER_OPTIMIZE=1
 export DXVK_DEBUG_LAYERS=0
@@ -52,24 +54,36 @@ export DXVK_SHADER_READ_PATH="/tmp"
 export DXVK_LOG_LEVEL=none
 #export DXVK_HUD=fps,version,compiler
 #LD_PRELOAD=”libpthread.so.0 libGL.so.1″
-export __GL_THREADED_OPTIMIZATIONS=1
-export __GL_YIELD=NOTHING
-export PULSE_LATENCY_MSEC=60
+#export PULSE_LATENCY_MSEC=60
+export KWIN_TRIPLE_BUFFER=1
+export TRIPLE_BUFFER=1
+export XVideoTextureSyncToVBlank=0
+export MESA_NO_ERROR=1
+#export AMDVLK_ENABLE_DEVELOPING_EXT="all"
 export __GL_SHADER_DISK_CACHE=1
 export __GL_SHADER_DISK_CACHE_PATH="/tmp"
 export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
-#export PBA=0
+export __GL_YIELD="NOTHING"
+export vblank_mode=0
+export __GL_SYNC_TO_VBLANK=0
+export STEAM_RUNTIME_HEAVY=1
+export __NV_PRIME_RENDER_OFFLOAD=1
+#export __GLX_VENDOR_LIBRARY_NAME=nvidia
+#export __VK_LAYER_NV_optimus=NVIDIA_only
+#export RADV_PERFTEST=aco
+export RADV_PERFTEST=llvm
+export __GL_THREADED_OPTIMIZATIONS=1
+export mesa_glthread=true
 export PBA_DISABLE=0
-export DXVK_HUD=compiler,fps
+export DXVK_HUD=fps
 glxinfo -B
 glxgears -stereo > /dev/null 2>&1
 
+# Para GAMEMODE: gamemoderun
+#export LD_PRELOAD="$LD_PRELOAD:/usr/\$LIB/libgamemodeauto.so.0"
+
 # Primeiro configurar o wine
 #"$W"/bin/winecfg
-
-#~/.jogos/scripts/winetricks -q vcrun2017
-#~/.jogos/scripts/winetricks -q win10
-
 
 #⛔⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⛔
 #  _          _                     _ _       _                       _           _
@@ -131,6 +145,7 @@ cd "/home/$USER/.jogos/wineprefixes/goggalaxy/drive_c/Program Files (x86)/GOG Ga
 
 
 
+
 ################################# 喇 MPV Player 懶 喇
 #pkill -9 .exe
 #cd ~/.jogos/setups/
@@ -139,6 +154,10 @@ cd "/home/$USER/.jogos/wineprefixes/goggalaxy/drive_c/Program Files (x86)/GOG Ga
 #cd ~/.jogos/setups/mpv
 # "$W"/bin/wineconsole mpv-install.bat
 
+################################# Finalização
+#~/.jogos/scripts/winetricks vd=1360x768
+
+rm -rf ~/.local/share/applications/*wine*
 
 ################################# Opções extras:
 # Opções da "$GN": https://developer.valvesoftware.com/wiki/Command_Line_Options
@@ -218,6 +237,18 @@ cd "/home/$USER/.jogos/wineprefixes/goggalaxy/drive_c/Program Files (x86)/GOG Ga
 
 ################################# Oções descartadas:
 
+#export VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay
+#export VK_LAYER_MESA_OVERLAY_STATS=submit,draw,pipeline-graphics
+#export VK_LAYER_MESA_OVERLAY_POSITION=top-right
+#export vulkanpresentmode=0
+#export VkPresentModeKHR=0
+#export VK_PRESENT_MODE_IMMEDIATE_KHR=0
+#export VK_PRESENT_MODE_MAILBOX_KHR=0
+#export VK_PRESENT_MODE_FIFO_KHR=0
+
+#export OGL_FORCE_BLIT_ON
+#export OGL_DEFAULT_SWAP_INTERVAL_FORCE_OFF
+
 #cd ~/.local/share/applications
 #rm -rf wine*
 
@@ -228,8 +259,9 @@ cd "/home/$USER/.jogos/wineprefixes/goggalaxy/drive_c/Program Files (x86)/GOG Ga
 
 # INSTALE O DXVK - Manualmente
 
-#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x64/* ~/.jogos/wineprefixes/"$GN"/drive_c/windows/system32/
-#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x32/* ~/.jogos/wineprefixes/"$GN"/drive_c/windows/syswow64/
+#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x64/* ~/.jogos/wineprefixes/Origin/drive_c/windows/system32/
+#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x32/* ~/.jogos/wineprefixes/Origin/drive_c/windows/syswow64/
 
 #"$W"/bin/wine regsvr32 /i /S l3codecx.ax
 #~/.jogos/scripts/winetricks d3d10=native d3d10_1=native d3d10core=native d3d11=native dxgi=native
+rm -rf ~/.local/share/applications/*wine*
