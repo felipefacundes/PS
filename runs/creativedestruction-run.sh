662144bb8a2e75d8b52499d69bc667ab98e13a34
Felipe Facundes <felipe.facundes@gmail.com>
2020-01-21 20:36:59 -0300

    Update creativedestruction-run.sh

--- runs/creativedestruction-run.sh
+++ runs/creativedestruction-run.sh
@@ -31,8 +31,8 @@ export LD_LIBRARY64_PATH="$W/lib:$LD_LIBRARY64_PATH"
 export LD_LIBRARY_PATH="$W/lib:$LD_LIBRARY_PATH"
 #"$W"/bin/wineconsole "cmd"
 
-#export WINEDEBUG=-all,fps
-export WINEDEBUG=-all
+export WINEDEBUG=-all,fps
+#export WINEDEBUG=-all
 # Prefix do wine, destino do prefix individual para cada jogo é melhor e evita futuras falhas
 export WINEPREFIX=~/.jogos/wineprefixes/"$GN"
 # Para tornar a prefix do wine preparada para 32bits ou 64bits. Opção necessária para alguns jogos:
