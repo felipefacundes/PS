50812e4f70494c665eb079f2aebebd0348279f88
Felipe Facundes <felipe.facundes@gmail.com>
2020-01-28 02:44:56 -0300

    Update flatout2.sh

--- flatout2.sh
+++ flatout2.sh
@@ -30,6 +30,7 @@ rm -rf "$GN"
 cd ~/.jogos/scripts/run/
 rm -rf "$GN"-run.sh
 wget -nc https://raw.githubusercontent.com/felipefacundes/PS/master/runs/"$GN"-run.sh > /dev/null 2>&1
+wget -nc https://raw.githubusercontent.com/felipefacundes/PS/master/runs/"$GN"-gameranger-run.sh > /dev/null 2>&1
 chmod +x "$GN"-run.sh
 cd ~/.jogos/icons/
 wget -nc https://raw.githubusercontent.com/felipefacundes/PS/master/icons/"$GN".png > /dev/null 2>&1
