94e16ba29bfef92957c0358c5d7a2c01730d0b14
Felipe Facundes <felipe.facundes@gmail.com>
2020-01-28 15:31:35 -0300

    Update flatout2-run.sh

--- runs/flatout2-run.sh
+++ runs/flatout2-run.sh
@@ -98,9 +98,8 @@ glxgears -stereo > /dev/null 2>&1
 # Respeitando as Letras MAÍUSCULAS e minúsculas. Exemplo: Setup.exe
 #⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬
 
-#cd "/home/$USER/.jogos/wineprefixes/flatout2/drive_c/GOG Games/FlatOut 2"
-cd "/home/$USER/.jogos/wineprefixes/$GN/drive_c/flatout2_demo"
-"$W"/bin/wine FlatOut2.exe
+cd "/home/$USER/.jogos/wineprefixes/$GN/drive_c/FlatOut 2"
+"$W"/bin/wine FlatOut2.exe 2>&1 | tee /dev/stderr | sed -u -n -e '/trace/ s/.*approx //p' | osd_cat --lines=1 --color=yellow --outline=1 --pos=top --align=left
 
 #⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫
 ### Só altere essas DUAS linhas ACIMA, como já explicado.
