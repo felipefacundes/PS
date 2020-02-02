62d190513b5409f0203601390844bb100c985e32
Felipe Facundes <felipe.facundes@gmail.com>
2020-02-02 01:38:20 -0300

    Update EpicGamesStore.sh

--- EpicGamesStore.sh
+++ EpicGamesStore.sh
@@ -297,7 +297,7 @@ tar -xf Epic_Games.tar.zst
 cd ~/.jogos/setups/
 #wget -nc https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UplayInstaller.exe
 wget -nc https://www.opencode.net/felipefacundes/free-games/raw/master/UplayInstaller.exe
-"$W"/bin/wine UplayInstaller.exe
+"$W"/bin/wine UplayInstaller.exe /S
 #wget -nc "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi" -O EpicGamesLauncherInstaller.msi
 #"$W"/bin/msiexec /i EpicGamesLauncherInstaller.msi /q
 
