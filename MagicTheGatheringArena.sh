08c45649b365e82c2dc2886a2f74eab3110f32cf
Felipe Facundes <felipe.facundes@gmail.com>
2020-02-23 15:14:50 -0300

    Update MagicTheGatheringArena.sh

--- MagicTheGatheringArena.sh
+++ MagicTheGatheringArena.sh
@@ -182,7 +182,7 @@ ps ax|egrep '*\.exe'|grep -v 'egrep'|awk '{print $1 }' | xargs kill -9 $1 ; pkil
 ~/.jogos/scripts/winetricks -q comctl32ocx > /dev/null 2>&1
 ~/.jogos/scripts/winetricks -q comdlg32ocx > /dev/null 2>&1
 ~/.jogos/scripts/winetricks -q mf richtx32 winhttp msftedit mshflxgd msflxgrd msdelta > /dev/null 2>&1
-~/.jogos/scripts/winetricks -q corefonts d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus > /dev/null 2>&1
+~/.jogos/scripts/winetricks --unattended corefonts d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus > /dev/null 2>&1
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "VAMOS LÁ. VOCÊ CONSEGUE. Aguarde só MAIS UM POUCO."
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
