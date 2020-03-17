f884007fc283f3d254178f07b827e18962800575
Felipe Facundes <felipe.facundes@gmail.com>
2020-03-17 17:12:39 -0300

    Update GTAV.sh

--- GTAV.sh
+++ GTAV.sh
@@ -10,7 +10,7 @@ clear -T "$TERM"
 # Faça o seu pedido de tutorial e GamePlay no nosso:
 # 既 Grupo 調 Gamer do 切 Telegram 切: https://t.me/winehq_linux
 ########### Este script irá usar o wine personalizado. Mas, você poderá usar um wine na versão e local de sua escolha
-WV=Wine-PlayOnGit-st-5.2
+WV=Wine-PlayOnGit-5.4
 GN=GTAV
 SN="GTA V"
 CME="Jogo de Ação e Aventura"
