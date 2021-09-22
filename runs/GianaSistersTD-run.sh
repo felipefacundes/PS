2811de069d11c6ca2b187608cb355c052a3a64ff
Felipe Facundes <felipe.facundes@gmail.com>
2021-09-22 17:10:41 -0300

    Update GianaSistersTD-run.sh

--- runs/GianaSistersTD-run.sh
+++ runs/GianaSistersTD-run.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
+# Manteiner: Felipe Facundes
+# License: GPLv3
+# Email: playongit@gmail.com
+#  Telegram: @FeFacundes
+#  Telegram Group: t.me/winehq_linux
 rm -rf ~/.local/share/applications/*wine*
-# PlayOnGit - Inicie seus Jogos direto do menu iniciar, sem precisar de PlayOnLinux, Proton ou Lutris, e com um desempenho muito melhor e superior.
-# Licença: GPLv3
-# Mantenedor: Felipe Facundes
-# Faça o seu pedido de tutorial e GamePlay no nosso:
-# 既 Grupo 調 Gamer do 切 Telegram 切: https://t.me/winehq_linux
-########### Este script irá usar o wine personalizado. Mas, você poderá usar um wine na versão e local de sua escolha
-WV=wine-staging-5.1-1-x86_64
+ps ax|egrep '*.exe'|grep -v 'egrep'|awk '{print $1 }' | xargs kill -9 $1 ; pkill -9 .exe
+clear -T "$TERM"
+
+WV=wine-staging-6.16-1-x86_64
 GN=GianaSistersTD
 SN="Giana Sisters: Twisted Dreams"
 CME="Ação e aventura"
 
-#A# Essa é a versão escolhida do Wine
 export TERM=xterm
-# Para ver o FPS na tela e o uso da CPU, inclua cpu,fps
-#export GALLIUM_HUD="simple,fps"
-W=~/.jogos/wines/"$WV"
+W=~/.PlayOnGit/wines/"$WV"
 export WINE64="$W"/bin/wine64
 export WINE="$W"/bin/wine
 export WINEVERPATH="$W"
@@ -29,232 +28,180 @@ export WINEDLLPATH="$W"/lib/wine/fakedlls
 export LD_LIBRARY32_PATH="$W/lib32:$LD_LIBRARY32_PATH"
 export LD_LIBRARY64_PATH="$W/lib:$LD_LIBRARY64_PATH"
 export LD_LIBRARY_PATH="$W/lib:$LD_LIBRARY_PATH"
-#"$W"/bin/wineconsole "cmd"
+export WINEDEBUG=-all,fps
+export WINEPREFIX=~/.PlayOnGit/wineprefixes/"$GN"
+export WINEARCH=win64
+export WINEESYNC=1
+Wtricks=~/.PlayOnGit/scripts/winetricks
+#export MESA_GLSL_VERSION_OVERRIDE=440
+#export MESA_GL_VERSION_OVERRIDE=4.4COMPAT
 
-#export WINEDEBUG=-all,fps
-export WINEDEBUG=-all
-# Prefix do wine, destino do prefix individual para cada jogo é melhor e evita futuras falhas
-export WINEPREFIX=~/.jogos/wineprefixes/"$GN"
-# Para tornar a prefix do wine preparada para 32bits ou 64bits. Opção necessária para alguns jogos:
-export WINEARCH=win32
-export WINEESYNC=0
-#export ESYNC=0
-# Esta é uma opção que às vezes é necessária para alguns jogos   MESA_GL_VERSION_OVERRIDE=version
-#export MESA_GLSL_VERSION_OVERRIDE=450
-#export MESA_GL_VERSION_OVERRIDE=4.5COMPAT
-# Para placas gráficas híbridas use o DRI_PRIME=1
-#export DRI_PRIME=1
-#Origin.exe,OriginClientService.exe,
-#export WINEDLLOVERRIDES=d3d10,d3d11,dxgi=n
 export DXVK_SPIRV_OPT=ON
 export DXVK_SHADER_OPTIMIZE=1
 export DXVK_DEBUG_LAYERS=0
 export DXVK_SHADER_DUMP_PATH="/tmp"
-export DXVK_SHADER_READ_PATH="/tmp"
+export DXVK_STATE_CACHE_PATH="$WINEPREFIX"
+export DXVK_SHADER_READ_PATH="$WINEPREFIX"
+export DXVK_CONFIG_FILE="$WINEPREFIX"/dxvk.conf
 export DXVK_LOG_LEVEL=none
-#export DXVK_HUD=fps,version,compiler
-#LD_PRELOAD=”libpthread.so.0 libGL.so.1″
-#export PULSE_LATENCY_MSEC=60
 export KWIN_TRIPLE_BUFFER=1
 export TRIPLE_BUFFER=1
 export XVideoTextureSyncToVBlank=0
 export MESA_NO_ERROR=1
-#export AMDVLK_ENABLE_DEVELOPING_EXT="all"
 export GPU_FORCE_64BIT_PTR=1
 export GPU_MAX_HEAP_SIZE=100
 export GPU_USE_SYNC_OBJECTS=1
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
+export __GL_DXVK_OPTIMIZATIONS=1
 export __GL_SHADER_DISK_CACHE=1
-export __GL_SHADER_DISK_CACHE_PATH="/tmp"
+export __GL_SHADER_DISK_CACHE_PATH="$WINEPREFIX"
+export MESA_GLSL_CACHE_DIR="$WINEPREFIX"
 export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
 export __GL_YIELD="NOTHING"
 export vblank_mode=0
 export __GL_SYNC_TO_VBLANK=0
 export STEAM_RUNTIME_HEAVY=1
-export __NV_PRIME_RENDER_OFFLOAD=1
+
+# Nvidia Hybrid Card
+#export __NV_PRIME_RENDER_OFFLOAD=1
 #export __GLX_VENDOR_LIBRARY_NAME=nvidia
 #export __VK_LAYER_NV_optimus=NVIDIA_only
+
+# AMD Radeon Graphics Card
 #export RADV_PERFTEST=aco
-export RADV_PERFTEST=llvm
+
 export __GL_THREADED_OPTIMIZATIONS=1
 export mesa_glthread=true
 export PBA_DISABLE=0
+export WINE_LARGE_ADDRESS_AWARE=1
+export STAGING_SHARED_MEMORY=1
+#export STAGING_WRITECOPY=1
 export DXVK_HUD=fps
-glxinfo -B
+export DXVK_ASYNC=1
+glxinfo -B 2> /dev/null
 glxgears -stereo > /dev/null 2>&1
 
-# Para GAMEMODE: gamemoderun
+# GAMEMODE: gamemoderun
 #export LD_PRELOAD="$LD_PRELOAD:/usr/\$LIB/libgamemodeauto.so.0"
 
-# Primeiro configurar o wine
-#"$W"/bin/winecfg
+## Game dir and executable
+EXE="Steam.exe"
+cd "$WINEPREFIX/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Binaries/Win32"cd "/home/$USER/.jogos/wineprefixes/$GN/drive_c/Program Files/Steam/"
+## Executable Parameters
+Pr1="-SkipBuildPatchPrereq"
+Pr2="-opengl"
+Pr3="-gl"
+Pr4="-dx9"
+Pr5="-dx10"
+Pr6="-dx11"
 
-#⛔⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⛔
-#  _          _                     _ _       _                       _           _
-# | |        (_)                   | (_)     | |                     | |         (_)              ▏▏
-# | |     ___ _  __ _    __ _ ___  | |_ _ __ | |__   __ _ ___    __ _| |__   __ _ ___  _____      ▏▏
-# | |    / _ \ |/ _` |  / _` / __| | | | '_ \| '_ \ / _` / __|  / _` | '_ \ / _` | \ \/ / _ \     ▏▏
-# | |___|  __/ | (_| | | (_| \__ \ | | | | | | | | | (_| \__ \ | (_| | |_) | (_| | |>  < (_) |    ▏▏
-# \_____/\___|_|\__,_|  \__,_|___/ |_|_|_| |_|_| |_|\__,_|___/  \__,_|_.__/ \__,_|_/_/\_\___/     ⧩
-#⛔⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⛔
-# Logo abaixo é o CAMINHO do instalador do jogo e pode ser alterado por você, de acordo com às suas necessidades:
-# Coloque o endereço da PASTA entre às "ASPAS" e mude o nome do executável do instalador.
-# Respeitando as Letras MAÍUSCULAS e minúsculas. Exemplo: Setup.exe
-#⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬
+######## Zenity (Pseudo GUI) ########
+Game_Actions=`zenity \
+    --width=800 \
+    --height=550 \
+    --title='PlayOnGit Game Launcher and Settings' \
+    --list --text 'What do you want to do?' \
+    --radiolist --column 'Choice' \
+    --column 'Action' \
+    TRUE "Run ${SN}" \
+    FALSE WineConfig \
+    FALSE Winetricks \
+    FALSE 'Custom Wine executable (.exe)' \
+    FALSE 'Wine Uninstaller' \
+    FALSE 'Wine Regedit' \
+    FALSE 'Toggle DXVK (Disable/Enable)' \
+    FALSE 'Wineconsole (Wine CMD)' \
+    FALSE 'Kill all wine processes' \
+    FALSE 'Edit Script' \
+    FALSE 'Open Game Directory' \
+    FALSE 'Toggle Nvidia Hybrid Graphics | Use Nvidia for performance' \
+    FALSE 'Set your favorite terminal' \
+    FALSE 'Start your terminal' \
+    FALSE "Remove All Wineprefix ${SN}" \
+    FALSE Credits`
 
-cd "/home/$USER/.jogos/wineprefixes/$GN/drive_c/Program Files/Steam/"
-"$W"/bin/wine Steam.exe -applaunch 223220 -vulkan
-
-#⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫
-### Só altere essas DUAS linhas ACIMA, como já explicado.
-#⛔◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭◭⛔
-#   _        _                _
-#  | |   ___(_)__ _   __ _ __(_)_ __  __ _    ◭
-#  | |__/ -_) / _` | / _` / _| | '  \/ _` |   ▏▏
-#  |____\___|_\__,_| \__,_\__|_|_|_|_\__,_|   ▏▏
-#       ____
-#  _  _ ____ ____    _  _ ____ _  _ ____    _  _ ____ ____    ___  ____ _  _ ____ _ ____    _    _ _  _ _  _ ____ ____
-#  |\ | |__| |  |    |\/| |___  \/  |__|    |\ | |__| [__     |  \ |___ |\/| |__| | [__     |    | |\ | |__| |__| [__
-#  | \| |  | |__|    |  | |___ _/\_ |  |    | \| |  | ___]    |__/ |___ |  | |  | | ___]    |___ | | \| |  | |  | ___]
-#⛔ Não mexa nas demais linhas, deixa do jeito que está.                                                              ⛔
-
-# Irá abrir a localização e o script de inicialização do jogo:
-#xdg-open ~/.jogos/wineprefixes/"$GN"/drive_c/
-#xdg-open ~/.jogos/scripts/run/"$GN"-run.sh
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-################################# 喇 MPV Player 懶 喇
-#pkill -9 .exe
-#cd ~/.jogos/setups/
-#wget -nc https://www.opencode.net/felipefacundes/wine-bins/raw/master/codecs-and-players/mpv.tar.xz
-#tar -xf mpv.tar.xz
-#cd ~/.jogos/setups/mpv
-# "$W"/bin/wineconsole mpv-install.bat
-
-################################# Finalização
-#~/.jogos/scripts/winetricks vd=1360x768
-
-rm -rf ~/.local/share/applications/*wine*
-
-################################# Opções extras:
-# Opções da "$GN": https://developer.valvesoftware.com/wiki/Command_Line_Options
-#-insecure
-#-forcenovsync
-#-nojoy
-#-fullscreen ou -full
-#-applaunch 230410
-# Acima são opções da "$GN"
-#
-# programa.exe -opengl
-# progrma.exe -dx9
-#
-# Nosso grupo do Telegram para ajudar o pessoal a rodar jogos do Windows no Linux. E para fazer pedidos de tutorial:
-# https://t.me/winehq_linux
-# Canal do telegram. Para os arquivos: mfc42.dll e l3codecx.ax só efetuar a busca com o hashtag: #ageofempires
-# https://t.me/archLinux_pacotes
-# Tutorial do wine, todavia fiz alterações para funcionar corretamente:
-# https://appdb.winehq.org/objectManager.php?sClass=version&iId=29761&iTestingId=82749
-# Dlls links:
-# https://www.pconlife.com/download/winosfile/208/49/c4cce0cfb49fbcc0ad0596523839ccfc/
-# https://www.dlldump.com/download-dll-files_new.php/dllfiles/M/mfc42.dll/6.0.400/download.html
-# Codecs:  Baixe a versão Mega:  K-Lite_Codec_Pack_1494_Mega.exe  # Depois tem que configurar: às preferencias de codec e habilitar tudo.
-# https://www.codecguide.com/download_kl.htm
-
-#
-#                    ,cldxOxoc:;,
-#               ,;:okKNXKK0kO0Okxddol:;,
-#        ,;codxkkOKXKko:'......,;clx0KXXOxol:,
-#   :lodxxdololc:,'..................,cdk00kxkOkxoc,
-#  ;XOxdl:,..  .......................... ..,;lx0XNx
-#  lX0c      ..;dddddooooollll,............     .0XO'
-#  oKK;    ....0NNXXKKK00OOOkx'.............     x0x,
-#  lXX;   ....,WWNNNKdddoooooc.'..............   okk,
-#  cXW:  .....xWWWWWl''''''''''''''............  dd0,
-#  :OWo ......NMMMMWOkkkkxxxc''''''''.......... .kxK'
-#  ,dKk .....lWWWWMMMWWWNNNX:,''''''''...........KOx'
-#  ,lk0......ONNNWWddddddddo,,,,,''''''.........lNOc'
-#  'cdk;....;KXXXNO''',,,,,,,ddddoooolllccc:....0WO,'
-#  ',ddd....o0KKKXl'',,,,,,,lWWNNNXXKKK00OOc...,0Xx'
-#   'llo;...kO00K0,'',,,,,,,0MWWWN0OOOkkkxx'...oxK;'
-#   ',xcl..,lloool''',,,,,,:WWMMMX''''''......cxko'
-#    ':x:c......''''''',,,,xNWWWM0dddddool...:00O,'
-#     'ld::.......'''''',,,KNNNWWWMMMWWWWx..;XXK;'
-#      'dd::.......'''''''lKXXNN0kkOOOkkk;.;0KK;'
-#       'ox::........'''''x0KKKX;'''......lK0k;'
-#        'cx:c,........'.,OO00KO........'kN0o,'
-#         ',dllc'........lkkOO0c.......:KNk:'
-#           ':lldc.......oodddd'.....,d0Oc,'
-#            ',:ldxl'..............'lxxo;,'
-#              ',;lx0x;..........,cldo:'
-#               '',cxXXd,....,cdxkd:'
-#                   ',視嗢荒華者喝啕喙調﬏磌,'
-#                     ',;lxdl:,'
-#
-# Tutorial by:
-#  ___    _             ___                     _
-# | __|__| (_)_ __  ___| __|_ _ __ _  _ _ _  __| |___ ___
-# | _/ -_) | | '_ \/ -_) _/ _` / _| || | ' \/ _` / -_|_-<
-# |_|\___|_|_| .__/\___|_|\__,_\__|\_,_|_||_\__,_\___/__/
-#            |_|
-#
-# ┏┓
-# ┃┃╱╲ nesta
-# ┃╱╱╲╲ casa
-# ╱╱╭╮╲╲ todos
-# ▔▏┗┛▕▔ usam
-# ╱▔▔▔▔▔▔▔▔▔▔╲
-# LINUX
-# ╱╱┏┳┓╭╮┏┳┓ ╲╲
-# ▔▏┗┻┛┃┃┗┻┛▕▔
-# -------------------------
-
-
-
-
-
-################################# Oções descartadas:
-
-#cd ~/.local/share/applications
-#rm -rf wine*
-
-#cd ~/.jogos/wineprefixes/"$GN"/drive_c/windows/system32/
-#rm ntdll.dll
-#wget -nc https://www.dlldump.com/dllfiles/N/ntdll.dll
-# "$W"/bin/wine regsvr32 /i /S ntdll.dll
-
-# INSTALE O DXVK - Manualmente
-
-#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x64/* ~/.jogos/wineprefixes/Origin/drive_c/windows/system32/
-#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x32/* ~/.jogos/wineprefixes/Origin/drive_c/windows/syswow64/
-
-#"$W"/bin/wine regsvr32 /i /S l3codecx.ax
-#~/.jogos/scripts/winetricks d3d10=native d3d10_1=native d3d10core=native d3d11=native dxgi=native
-rm -rf ~/.local/share/applications/*wine*
+if [ "$Game_Actions" = "Run ${SN}" ] ; then
+    "$W"/bin/wine "$EXE" -dx11 -applaunch 223220 \
+    2>&1 | tee /dev/stderr | sed -u -n -e \
+    '/trace/ s/.*approx //p' | osd_cat --lines=1 \
+    --color=yellow --outline=1 --pos=top --align=left
+fi
+if [ "$Game_Actions" = "WineConfig" ] ; then
+    "$W"/bin/winecfg
+fi
+if [ "$Game_Actions" = "Winetricks" ] ; then
+    "$Wtricks"
+fi
+if [ "$Game_Actions" = "Custom Wine executable (.exe)" ] ; then
+    Cust_EXE=`zenity --file-selection --filename="$WINEPREFIX/drive_c/" \
+    --text "Custom Wine executable (.exe)" --title "Open executable (.exe)"`
+    "$W"/bin/wine "$Cust_EXE"
+fi
+if [ "$Game_Actions" = "Wine Uninstaller" ] ; then
+    "$W"/bin/wine uninstaller
+fi
+if [ "$Game_Actions" = "Wine Regedit" ] ; then
+    "$W"/bin/wine regedit
+fi
+if [ "$Game_Actions" = "Toggle DXVK (Disable/Enable)" ] ; then
+    toggle_dxvk_check=~/.PlayOnGit/scripts/functions/"$GN"-toggle-dxvk-check
+    if [ ! -e "$toggle_dxvk_check" ] ; then
+        touch ~/.PlayOnGit/scripts/functions/"$GN"-toggle-dxvk-check
+        echo "DXVK Disable" > ~/.PlayOnGit/scripts/functions/"$GN"-toggle-dxvk-check
+        "$Wtricks" d3d9=default d3d10=default d3d10_1=default d3d10core=default d3d11=default > /dev/null 2>&1
+        zenity --info --ellipsize --title="Toggle DXVK" --text "DXVK Disable"
+    else
+        rm ~/.PlayOnGit/scripts/functions/"$GN"-toggle-dxvk-check
+        "$Wtricks" d3d9=native d3d10=native d3d10_1=native d3d10core=native d3d11=native > /dev/null 2>&1
+        zenity --info --ellipsize --title="Toggle DXVK" --text "DXVK Enable"
+    fi
+fi
+if [ "$Game_Actions" = "Wineconsole (Wine CMD)" ] ; then
+    cd "$WINEPREFIX"/drive_c/
+    "$W"/bin/wineconsole
+fi
+if [ "$Game_Actions" = "Kill all wine processes" ] ; then
+    ps ax|egrep '*.exe'|grep -v 'egrep'|awk '{print $1 }' | xargs kill -9 $1 ; pkill -9 .exe
+fi
+if [ "$Game_Actions" = "Edit Script" ] ; then
+    xdg-open ~/.PlayOnGit/scripts/run/"$GN"-run.sh
+fi
+if [ "$Game_Actions" = "Open Game Directory" ] ; then
+    xdg-open "$WINEPREFIX"/drive_c
+fi
+if [ "$Game_Actions" = "Toggle Nvidia Hybrid Graphics | Use Nvidia for performance" ] ; then
+    cd ~/.PlayOnGit/scripts/functions
+    ./"$GN"-Toggle_Nvidia.sh
+fi
+if [ "$Game_Actions" = "Set your favorite terminal" ] ; then
+    Set_Terminal=`zenity --entry --text \
+    "Set your favorite terminal. Example: kitty, xterm, etc..." \
+    --title "Set your favorite terminal"`
+    touch ~/.PlayOnGit/scripts/run/TERM.conf
+    echo "$Set_Terminal" > ~/.PlayOnGit/scripts/run/TERM.conf
+    chmod +x  ~/.PlayOnGit/scripts/run/TERM.conf
+fi
+if [ "$Game_Actions" = "Start your terminal" ] ; then
+    cd "$WINEPREFIX"/drive_c/
+    shopt -s expand_aliases
+    alias winetricks=~/.PlayOnGit/scripts/winetricks
+    ~/.PlayOnGit/scripts/run/TERM.conf
+fi
+if [ "$Game_Actions" = "Remove All Wineprefix ${SN}" ] ; then
+    Del_Prefix=`zenity --width=750 --height=200 \
+    --title="Remove All Wineprefix ${SN}?" --list \
+    --text "Remove All Wineprefix ${SN}?" --radiolist \
+    --column 'Choice' --column 'Action' TRUE No FALSE Yes`
+   if [ "$Del_Prefix" = "Yes" ] ; then
+    rm -f /home/"$USER"/.local/share/applications/"$GN".desktop
+    rm -rf /home/"$USER"/.PlayOnGit/wineprefixes/"$GN"/
+    rm -f /home/"$USER"/.PlayOnGit/scripts/run/"$GN"-run.sh
+    rm -f /home/"$USER"/.PlayOnGit/scripts/functions/"$GN"-Toggle_Nvidia.sh
+   fi
+fi
+if [ "$Game_Actions" = "Credits" ] ; then
+    zenity --width=240 --height=200 --info \
+    --title="Credits" --text="Manteiner: Felipe Facundes Email: playongit@gmail.com License: GPLv3"
+fi
+exit 0
