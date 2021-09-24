7a47e478f4828ef6ab4431128f4785f22ac6efc7
Felipe Facundes <felipe.facundes@gmail.com>
2021-09-23 22:37:29 -0300

    Update EpicGamesStore.sh

--- EpicGamesStore.sh
+++ EpicGamesStore.sh
@@ -12,7 +12,7 @@ rm -rf ~/.local/share/applications/*wine*
 whiptail --msgbox "Installation may take some time depending on the GAME. Above all, please: PATIENCE. WAIT! You will be notified when installation is complete." 10 30 
 whiptail --msgbox "A instalação poderá demorar dependendo do JOGO. Acima de tudo tenha: PACIÊNCIA. AGUARDE! Você será notificado, quando a instalação concluir." 10 30
 
-WV=wine-staging-6.16-1-x86_64
+WV=wine-tkg-staging-6.17.r13-x86_64
 GN=EpicGamesStore
 SN="Epic Games Store"
 CME="Loja da Epic Games"
