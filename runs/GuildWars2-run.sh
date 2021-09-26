30b2aa3559c94a9298afdd2096a6018b2a600ee3
Felipe Facundes <felipe.facundes@gmail.com>
2021-09-26 00:40:57 -0300

    Update GuildWars2-run.sh

--- runs/GuildWars2-run.sh
+++ runs/GuildWars2-run.sh
@@ -127,7 +127,7 @@ Game_Actions=`zenity \
     FALSE Credits`
 
 if [ "$Game_Actions" = "Run ${SN}" ] ; then
-    "$W"/bin/wine "$EXE" \
+    "$W"/bin/wine "$EXE" -dx9single -autologin \
     2>&1 | tee /dev/stderr | sed -u -n -e \
     '/trace/ s/.*approx //p' | osd_cat --lines=1 \
     --color=yellow --outline=1 --pos=top --align=left
