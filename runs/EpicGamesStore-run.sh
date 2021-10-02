0c2cbcc0eb47bfc358e0f851c4f46613066f0ae2
Felipe Facundes <felipe.facundes@gmail.com>
2021-10-02 02:12:37 -0300

    Update EpicGamesStore-run.sh

--- runs/EpicGamesStore-run.sh
+++ runs/EpicGamesStore-run.sh
@@ -183,7 +183,7 @@ if [ "$Game_Actions" = "Choose another version of Wine" ] ; then
         --progress --pulsate --auto-close --title="PlayOnGit Wine Download" --text="<b>Download</b> in progress:"
         tar -xf "$NWV".tar.zst 2>&1 | zenity \
         --progress --pulsate --auto-close --title="Extracting Wine!" --text="Extracting Wine!"
-        AWV=`cat "$Script_Run" | head -n 18 | grep -i WV= | cut -c 4-90`
+        AWV=`cat "$Script_Run" | head -n 17 | grep -i WV= | cut -c 4-90`
         zenity --info --ellipsize --title="Success!" --text "<b>Now the new version of Wine is:</b>\n\n$AWV\n\nfor $SN"
         rm -f ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     fi
