8b7df4a941473dff9982feffccf4f1d05cd8f138
Felipe Facundes <felipe.facundes@gmail.com>
2021-10-08 18:32:16 -0300

    Update ageofempiresIII-run.sh

--- runs/ageofempiresIII-run.sh
+++ runs/ageofempiresIII-run.sh
@@ -138,7 +138,7 @@ Game_Actions=`zenity \
     FALSE Credits`
 
 if [ "$Game_Actions" = "Run ${SN}" ] ; then
-    "$W"/bin/wine "$EXE" -dx11 -applaunch "$Steam_Game_ID" \
+    "$W"/bin/wine "$EXE" \
     2>&1 | tee /dev/stderr | sed -u -n -e \
     '/trace/ s/.*approx //p' | osd_cat --lines=1 \
     --color=yellow --outline=1 --pos=top --align=left
