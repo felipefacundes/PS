2bf6085e0bb3c82af887d981c5970c4f9881503d
Felipe Facundes <felipe.facundes@gmail.com>
2021-10-08 13:50:06 -0300

    Update LifeAfter-run.sh

--- runs/LifeAfter-run.sh
+++ runs/LifeAfter-run.sh
@@ -138,7 +138,7 @@ Game_Actions=`zenity \
     FALSE Credits`
 
 if [ "$Game_Actions" = "Run ${SN}" ] ; then
-    "$W"/bin/wine start /unix "$EXE" \
+    watch "$W"/bin/wine start /unix "$EXE" \
     2>&1 | tee /dev/stderr | sed -u -n -e \
     '/trace/ s/.*approx //p' | osd_cat --lines=1 \
     --color=yellow --outline=1 --pos=top --align=left
