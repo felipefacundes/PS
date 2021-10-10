9163864fa52cceab77e25c9ea42f291bab812c53
Felipe Facundes <felipe.facundes@gmail.com>
2021-10-10 16:59:49 -0300

    Update GTAV-run.sh

--- runs/GTAV-run.sh
+++ runs/GTAV-run.sh
@@ -14,7 +14,7 @@ Wkill
 rm -rf ~/.local/share/applications/*wine*
 clear -T "$TERM"
 
-WV=wine-tkg-staging-6.18.r5-x86_64
+WV=wine-tkg-staging-6.17.r13-x86_64
 GN=GTAV
 SN="Grand Theft Auto V"
 CME="Grand Theft Auto V is an action-adventure video game developed by Rockstar."
@@ -96,10 +96,10 @@ EXE0="EpicGamesLauncher.exe"
 DIR0="$WINEPREFIX/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Binaries/Win32"
 EXE1="Steam.exe"
 DIR1="$WINEPREFIX/drive_c/Program Files (x86)/Steam/"
-EXE2="Launcher.exe"
-DIR2="$WINEPREFIX/drive_c/Program Files/Rockstar Games/Launcher"
-EXE3="GTAVLauncher.exe"
-DIR3="$WINEPREFIX/drive_c/Program Files (x86)/Steam/steamapps/common/Grand Theft Auto V"
+EXE2="GTAVLauncher.exe"
+DIR2="$WINEPREFIX/drive_c/Program Files (x86)/Steam/steamapps/common/Grand Theft Auto V"
+EXE3="Launcher.exe"
+DIR3="$WINEPREFIX/drive_c/Program Files/Rockstar Games/Launcher"
 ## Executable Parameters
 Pr1="-SkipBuildPatchPrereq"
 Pr2="-opengl"
@@ -114,11 +114,12 @@ Pr10="-d3d12"
 Pr11="-vulkan"
 
 ######## Zenity (Pseudo GUI) ########
+Script_Run=~/.PlayOnGit/scripts/run/"$GN"-run.sh
 Game_Actions=`zenity \
     --width=800 \
-    --height=690 \
+    --height=650 \
     --title='PlayOnGit Game Launcher and Settings' \
-    --list --text 'What do you want to do?' \
+    --list --text "(PlayOnGit) ${SN} Menu. What do you want to do?" \
     --radiolist --column 'Choice' \
     --column 'Action' \
     TRUE "Run ${SN} (Epic Games Store)" \
@@ -141,6 +142,7 @@ Game_Actions=`zenity \
     FALSE 'Toggle Nvidia Hybrid Graphics | Use Nvidia for performance' \
     FALSE 'Set your favorite terminal' \
     FALSE 'Start your terminal' \
+    FALSE 'Create your customized script, to run your game or other app!' \
     FALSE "Remove All Wineprefix ${SN}" \
     FALSE Credits`
 
@@ -158,13 +160,6 @@ if [ "$Game_Actions" = "Run ${SN} (Steam)" ] ; then
     '/trace/ s/.*approx //p' | osd_cat --lines=1 \
     --color=yellow --outline=1 --pos=top --align=left
 fi
-if [ "$Game_Actions" = "Rockstar Games Launcher" ] ; then
-    cd "$DIR2"
-    "$W"/bin/wine "$EXE2" \
-    2>&1 | tee /dev/stderr | sed -u -n -e \
-    '/trace/ s/.*approx //p' | osd_cat --lines=1 \
-    --color=yellow --outline=1 --pos=top --align=left
-fi
 if [ "$Game_Actions" = "Run ${SN} (Steam) directly" ] ; then
     cd "$DIR3"
     "$W"/bin/wine "$EXE3" -dx11 -applaunch 271590 \
@@ -172,40 +167,54 @@ if [ "$Game_Actions" = "Run ${SN} (Steam) directly" ] ; then
     '/trace/ s/.*approx //p' | osd_cat --lines=1 \
     --color=yellow --outline=1 --pos=top --align=left
 fi
+if [ "$Game_Actions" = "Rockstar Games Launcher" ] ; then
+    cd "$DIR2"
+    "$W"/bin/wine "$EXE2" \
+    2>&1 | tee /dev/stderr | sed -u -n -e \
+    '/trace/ s/.*approx //p' | osd_cat --lines=1 \
+    --color=yellow --outline=1 --pos=top --align=left
+fi
 if [ "$Game_Actions" = "WineConfig" ] ; then
     "$W"/bin/winecfg
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Winetricks" ] ; then
     "$Wtricks"
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Custom Wine executable (.exe)" ] ; then
     Cust_EXE=`zenity --file-selection --directory --filename="$WINEPREFIX/drive_c/" \
     --text "Custom Wine executable (.exe)" --title "Open executable (.exe)"`
     "$W"/bin/wine "$Cust_EXE"
+    exec "$0"
 fi
 if [ "$Game_Actions" = "WineFile (Wine File Manager)" ] ; then
     cd "$WINEPREFIX/drive_c/"
     "$W"/bin/winefile
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Explorer++ (File Manager)" ] ; then
     "$W"/bin/wine Explorerpp c:
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Wine Uninstaller" ] ; then
     "$W"/bin/wine uninstaller
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Wine Regedit" ] ; then
     "$W"/bin/wine regedit
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Wineconsole (Wine CMD)" ] ; then
     cd "$WINEPREFIX"/drive_c/
     "$W"/bin/wineconsole
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Choose another version of Wine" ] ; then
     rm -f ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     bash <(curl -s https://raw.githubusercontent.com/felipefacundes/PS/master/other_scripts/wine_list.sh)
     if ls ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt > /dev/null 2>&1 ; then
         NWV=`cat ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt`
-        Script_Run=~/.PlayOnGit/scripts/run/"$GN"-run.sh
         cd ~/.PlayOnGit/wines/
         rm -rf "$NWV"
         rm -f "$NWV".tar.zst
@@ -214,10 +223,11 @@ if [ "$Game_Actions" = "Choose another version of Wine" ] ; then
         --progress --pulsate --auto-close --title="PlayOnGit Wine Download" --text="<b>Download</b> in progress:"
         tar -xf "$NWV".tar.zst 2>&1 | zenity \
         --progress --pulsate --auto-close --title="Extracting Wine!" --text="Extracting Wine!"
+        rm -f ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
         AWV=`cat "$Script_Run" | head -n 17 | grep -i WV= | cut -c 4-90`
         zenity --info --ellipsize --title="Success!" --text "<b>Now the new version of Wine is:</b>\n\n$AWV\n\nfor $SN"
-        rm -f ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     fi
+    source "$Script_Run"
 fi
 if [ "$Game_Actions" = "Toggle DXVK (Disable/Enable)" ] ; then
     toggle_dxvk_check=~/.PlayOnGit/scripts/functions/"$GN"-toggle-dxvk-check
@@ -233,19 +243,24 @@ if [ "$Game_Actions" = "Toggle DXVK (Disable/Enable)" ] ; then
         --progress --pulsate --auto-close --title="Enabling DXVK. Wait! Processing..." --text="<b>Enabling DXVK.</b>\n\n Wait! Processing..."
         zenity --info --ellipsize --title="Toggle DXVK" --text "DXVK <b>Enabled</b>"
     fi
+    source "$Script_Run"
 fi
 if [ "$Game_Actions" = "Kill all wine processes" ] ; then
     Wkill
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Edit Script" ] ; then
-    xdg-open ~/.PlayOnGit/scripts/run/"$GN"-run.sh
+    xdg-open "$Script_Run"
+    source "$Script_Run"
 fi
 if [ "$Game_Actions" = "Open Game Directory" ] ; then
     xdg-open "$WINEPREFIX"/drive_c
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Toggle Nvidia Hybrid Graphics | Use Nvidia for performance" ] ; then
     cd ~/.PlayOnGit/scripts/functions
     ./"$GN"-Toggle_Nvidia.sh
+    source "$Script_Run"
 fi
 if [ "$Game_Actions" = "Set your favorite terminal" ] ; then
     Set_Terminal=`zenity --entry --text \
@@ -254,28 +269,36 @@ if [ "$Game_Actions" = "Set your favorite terminal" ] ; then
     touch ~/.PlayOnGit/scripts/run/TERM.conf
     echo "$Set_Terminal" > ~/.PlayOnGit/scripts/run/TERM.conf
     chmod +x  ~/.PlayOnGit/scripts/run/TERM.conf
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Start your terminal" ] ; then
     cd "$WINEPREFIX"/drive_c/
     shopt -s expand_aliases
     alias winetricks=~/.PlayOnGit/scripts/winetricks
     ~/.PlayOnGit/scripts/run/TERM.conf
+    exec "$0"
+fi
+if [ "$Game_Actions" = "Create your customized script, to run your game or other app!" ] ; then
+    bash <(curl -s https://raw.githubusercontent.com/felipefacundes/PS/master/other_scripts/create_your_installation_script.sh)
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Remove All Wineprefix ${SN}" ] ; then
     Del_Prefix=`zenity --width=750 --height=200 \
     --title="Remove All Wineprefix ${SN}?" --list \
     --text "Remove All Wineprefix ${SN}?" --radiolist \
     --column 'Choice' --column 'Action' TRUE No FALSE Yes`
-   if [ "$Del_Prefix" = "Yes" ] ; then
-    rm -f /home/"$USER"/.local/share/applications/"$GN".desktop
-    rm -rf /home/"$USER"/.PlayOnGit/wineprefixes/"$GN"/
-    rm -f /home/"$USER"/.PlayOnGit/scripts/run/"$GN"-run.sh
-    rm -f /home/"$USER"/.PlayOnGit/scripts/functions/"$GN"-Toggle_Nvidia.sh
-   fi
+    if [ "$Del_Prefix" = "Yes" ] ; then
+        rm -f /home/"$USER"/.local/share/applications/"$GN".desktop
+        rm -rf /home/"$USER"/.PlayOnGit/wineprefixes/"$GN"/
+        rm -f /home/"$USER"/.PlayOnGit/scripts/run/"$GN"-run.sh
+        rm -f /home/"$USER"/.PlayOnGit/scripts/functions/"$GN"-Toggle_Nvidia.sh
+    fi
+    exec "$0"
 fi
 if [ "$Game_Actions" = "Credits" ] ; then
     zenity --info --ellipsize --title="Credits" \
     --text="Manteiner: Felipe Facundes\nEmail: playongit@gmail.com\nLicense: GPLv3"
+    exec "$0"
 fi
 exit 0
 ###################################################################
