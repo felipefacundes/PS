0365bcfd7d7975306e12c31da9ed73daa4ac4af4
Felipe Facundes <felipe.facundes@gmail.com>
2021-10-20 19:56:59 -0300

    Update HorizonZeroDawn.sh

--- HorizonZeroDawn.sh
+++ HorizonZeroDawn.sh
@@ -50,6 +50,7 @@ Test_Mirror_Sourceforge() {
 }
 Check_Wine_and_Get() {
     cd ~/.PlayOnGit/wines/
+    Get https://raw.githubusercontent.com/felipefacundes/PS/master/Wines_md5sum/"$WV".tar.zst.md5sum
     wine_integrity_check=`md5sum "$WV".tar.zst | awk '{ print $1 }'`
     wine_integrity_file=`cat "$WV".tar.zst.md5sum`
         if [ "$wine_integrity_check" = "$wine_integrity_file" ]; then
@@ -57,7 +58,6 @@ Check_Wine_and_Get() {
             echo 'Wine checked!'
         else
             rm -f "$WV".tar.zst
-            Get https://raw.githubusercontent.com/felipefacundes/PS/master/Wines_md5sum/"$WV".tar.zst.md5sum
             Test_Mirror_Sourceforge
             Get "$Mirror"/project/wine-bins/"$WV".tar.zst
         fi
