613bd28752f9ffc7a59c28b55eaf041726ec1ba6
Felipe Facundes <felipe.facundes@gmail.com>
2021-10-23 17:38:23 -0300

    Add files via upload

--- other_scripts/Toggle_Gamemode.sh
+++ other_scripts/Toggle_Gamemode.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 Toggle_GAMEMODE="$WINEPREFIX"/.Toggle_GAMEMODE.txt
 if [ ! -e "$Toggle_GAMEMODE" ]; then
+    touch "$Toggle_GAMEMODE"
     sed -i "/libgamemodeauto.so.0/s/^#//" "$Script_Run"
     notify-send "You need Feral GAMEMODE installed"
     notify-send "GAMEMODE" "Enabled"
 else
+    rm -f "$Toggle_GAMEMODE"
     sed -i "/libgamemodeauto.so.0/s/^/#/" "$Script_Run"
     notify-send "GAMEMODE" "Disabled"
 fi
--- other_scripts/Toggle_STAGING_WRITECOPY.sh
+++ other_scripts/Toggle_STAGING_WRITECOPY.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 STAGING_WRITECOPY="$WINEPREFIX"/.STAGING_WRITECOPY.txt
 if [ ! -e "$STAGING_WRITECOPY" ]; then
+    touch "$STAGING_WRITECOPY"
     sed -i "/export STAGING_WRITECOPY=1/s/^#//" "$Script_Run"
     notify-send "STAGING_WRITECOPY" "Enabled"
 else
+    rm -f "$STAGING_WRITECOPY"
     sed -i "/export STAGING_WRITECOPY=1/s/^/#/" "$Script_Run"
     notify-send "STAGING_WRITECOPY" "Disabled"
 fi
