508f162d7cb477e5250b1c8ce6830abaf71cf572
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-05 15:11:05 -0300

    Update wine_list.sh

--- other_scripts/wine_list.sh
+++ other_scripts/wine_list.sh
@@ -7,7 +7,10 @@ Wine_List=`
     --list --text 'What do you want to do?' \
     --radiolist --column 'Choice' \
     --column 'Action' \
-    TRUE "wine-tkg-staging-6.19.r8-x86_64" \
+    TRUE "wine-tkg-staging-6.20.r6-x86_64" \
+    FALSE "wine-tkg-staging-6.20.r1-x86_64" \
+    FALSE "wine-tkg-staging-6.20.r0-x86_64" \
+    FALSE "wine-tkg-staging-6.19.r8-x86_64" \
     FALSE "wine-tkg-staging-6.19.r6-x86_64" \
     FALSE "wine-tkg-staging-6.18.r5-x86_64" \
     FALSE "wine-tkg-staging-6.17.r13-x86_64" \
@@ -23,55 +26,67 @@ Wine_List=`
     FALSE "wine-staging-4.8-1-x86_64"
 `
 
-if [ "$Wine_List" = "wine-tkg-staging-6.19.r8-x86_64" ] ; then
+if [ "$Wine_List" = "wine-tkg-staging-6.20.r6-x86_64" ]; then
+    touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
+    echo "wine-tkg-staging-6.20.r6-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
+fi
+if [ "$Wine_List" = "wine-tkg-staging-6.20.r1-x86_64" ]; then
+    touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
+    echo "wine-tkg-staging-6.20.r1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
+fi
+if [ "$Wine_List" = "wine-tkg-staging-6.20.r0-x86_64" ]; then
+    touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
+    echo "wine-tkg-staging-6.20.r0-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
+fi
+if [ "$Wine_List" = "wine-tkg-staging-6.19.r8-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-tkg-staging-6.19.r8-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-tkg-staging-6.19.r6-x86_64" ] ; then
+if [ "$Wine_List" = "wine-tkg-staging-6.19.r6-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-tkg-staging-6.19.r6-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-tkg-staging-6.18.r5-x86_64" ] ; then
+if [ "$Wine_List" = "wine-tkg-staging-6.18.r5-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-tkg-staging-6.18.r5-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-tkg-staging-6.17.r13-x86_64" ] ; then
+if [ "$Wine_List" = "wine-tkg-staging-6.17.r13-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-tkg-staging-6.17.r13-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-6.16-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-6.16-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-6.16-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-6.14-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-6.14-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-6.14-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-6.6-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-6.6-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functionwine-staging-4.19-1-x86_64s/PlayOnGit_NWV.txt
     echo "wine-staging-6.6-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-5.21-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-5.21-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-5.21-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-5.1-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-5.1-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-5.1-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-4.21-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-4.21-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-4.21-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-4.20-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-4.20-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-4.20-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-4.16-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-4.16-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-4.16-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
-if [ "$Wine_List" = "wine-staging-4.8-1-x86_64" ] ; then
+if [ "$Wine_List" = "wine-staging-4.8-1-x86_64" ]; then
     touch ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
     echo "wine-staging-4.8-1-x86_64" > ~/.PlayOnGit/scripts/functions/PlayOnGit_NWV.txt
 fi
