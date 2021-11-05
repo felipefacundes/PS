bee2e0cfaeb9a29b491241ac3e3aa9be42514e3b
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-05 16:24:55 -0300

    Update NieR-Automata.sh

--- NieR-Automata.sh
+++ NieR-Automata.sh
@@ -19,7 +19,7 @@ rm -rf ~/.local/share/applications/*wine*
 whiptail --msgbox "Installation may take some time depending on the GAME. Above all, please: PATIENCE. WAIT! You will be notified when installation is complete." 10 30
 whiptail --msgbox "A instalação poderá demorar dependendo do JOGO. Acima de tudo tenha: PACIÊNCIA. AGUARDE! Você será notificado, quando a instalação concluir." 10 30
 
-export WV=wine-tkg-staging-6.19.r8-x86_64
+export WV=wine-tkg-staging-6.20.r6-x86_64
 export GN="NieR-Automata"
 export SN="NieR Automata"
 export CME="NieR: Automata tells the story of androids 2B, 9S and A2 and their battle to reclaim the machine-driven dystopia overrun by powerful machines."
