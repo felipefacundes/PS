f9a74505da85448610a410f1bf1775d8b72342b9
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-06 03:39:31 -0300

    Update Sekiro.sh

--- Sekiro.sh
+++ Sekiro.sh
@@ -19,7 +19,7 @@ rm -rf ~/.local/share/applications/*wine*
 whiptail --msgbox "Installation may take some time depending on the GAME. Above all, please: PATIENCE. WAIT! You will be notified when installation is complete." 10 30
 whiptail --msgbox "A instalação poderá demorar dependendo do JOGO. Acima de tudo tenha: PACIÊNCIA. AGUARDE! Você será notificado, quando a instalação concluir." 10 30
 
-export WV=wine-tkg-staging-6.19.r8-x86_64
+export WV=wine-tkg-staging-6.20.r6-x86_64
 export GN="Sekiro"
 export SN="Sekiro Shadows Die Twice"
 export CME="Carve your own clever path to vengeance in the critically acclaimed adventure from developer FromSoftware, creators of the Dark Souls series."
