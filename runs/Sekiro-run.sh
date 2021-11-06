303ec675bd4f806f377c6a06f272ca32ceb7e26e
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-06 03:40:13 -0300

    Update Sekiro-run.sh

--- runs/Sekiro-run.sh
+++ runs/Sekiro-run.sh
@@ -14,7 +14,7 @@ Wkill
 rm -rf ~/.local/share/applications/*wine*
 clear -T "$TERM"
 
-export WV=wine-tkg-staging-6.19.r8-x86_64
+export WV=wine-tkg-staging-6.20.r6-x86_64
 export GN="Sekiro"
 export SN="Sekiro Shadows Die Twice"
 export CME="Carve your own clever path to vengeance in the critically acclaimed adventure from developer FromSoftware, creators of the Dark Souls series."
