4fb068e038481002a825a5f0ba3572fd2565382c
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-08 11:57:00 -0300

    Update steam-run.sh

--- runs/steam-run.sh
+++ runs/steam-run.sh
@@ -14,7 +14,7 @@ Wkill
 rm -rf ~/.local/share/applications/*wine*
 clear -T "$TERM"
 
-export WV=wine-tkg-staging-6.19.r8-x86_64
+export WV=wine-tkg-staging-6.20.r6-x86_64
 export GN="steam"
 export SN="Steam Windows"
 export CME="Steam is the ultimate destination for playing, discussing, and creating games."
