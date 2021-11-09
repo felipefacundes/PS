e73937c8ec8eb7c9c1fca9504a120882aedabe11
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-09 11:43:04 -0300

    Update UbisoftConnect-run.sh

--- runs/UbisoftConnect-run.sh
+++ runs/UbisoftConnect-run.sh
@@ -14,7 +14,7 @@ Wkill
 rm -rf ~/.local/share/applications/*wine*
 clear -T "$TERM"
 
-export WV=wine-tkg-staging-6.19.r8-x86_64
+export WV=wine-tkg-staging-6.20.r6-x86_64
 export GN="UbisoftConnect"
 export SN="Ubisoft Connect"
 export CME="Ubisoft Connect - Ubisoft Digital Distributor."
