7c4493ed19c05cd8b2dc1dee148417c3190a6b6d
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-15 15:05:38 -0300

    Update Assassins_Creed_Syndicate.sh

--- Assassins_Creed_Syndicate.sh
+++ Assassins_Creed_Syndicate.sh
@@ -174,7 +174,8 @@ echo "━━━━━━━━━━━━━━━━━━━━━━━━�
 echo "TUDO dependerá do seu PROCESSADOR. Abaixo de 3GHz demorará BEM mais."
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
-"$Wtricks" -q corefonts d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus > /dev/null 2>&1
+"$W"/bin/wine uninstaller --remove {BEF75720-E23F-5A02-B01F-CE9B220A1B92} {B9CD3566-6115-5FD4-960F-59EF845316EC}
+"$Wtricks" -q faudio corefonts d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 > /dev/null 2>&1
 
 echo
 echo
@@ -187,36 +188,36 @@ echo "━━━━━━━━━━━━━━━━━━━━━━━━�
 echo "VAMOS LÁ. VOCÊ CONSEGUE. Aguarde só MAIS UM POUCO."
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
-"$Wtricks" -q vcrun2005 vcrun6sp6 > /dev/null 2>&1
-"$Wtricks" -q vb6run > /dev/null 2>&1
-"$Wtricks" -q vcrun2008 > /dev/null 2>&1
-"$Wtricks" -q mfc40 mfc42 > /dev/null 2>&1
+#"$Wtricks" -q vcrun2005 vcrun6sp6 > /dev/null 2>&1
+#"$Wtricks" -q vb6run > /dev/null 2>&1
+#"$Wtricks" -q vcrun2008 > /dev/null 2>&1
+#"$Wtricks" -q mfc40 mfc42 > /dev/null 2>&1
 echo "Progress ."
-"$Wtricks" -q vcrun2010 > /dev/null 2>&1
+#"$Wtricks" -q vcrun2010 > /dev/null 2>&1
 echo "Progress .."
-"$Wtricks" -q vcrun2012 > /dev/null 2>&1
-"$Wtricks" -q vcrun2013 > /dev/null 2>&1
+#"$Wtricks" -q vcrun2012 > /dev/null 2>&1
+#"$Wtricks" -q vcrun2013 > /dev/null 2>&1
 echo "Progress ..."
-"$Wtricks" -q mfc140 > /dev/null 2>&1
-"$Wtricks" -q vcrun2015 > /dev/null 2>&1
+#"$Wtricks" -q mfc140 > /dev/null 2>&1
+#"$Wtricks" -q vcrun2015 > /dev/null 2>&1
 #"$Wtricks" -q --force vcrun2017 > /dev/null 2>&1
 echo "Progress ...."
 "$Wtricks" autostart_winedbg=disabled nvapi=disabled nvapi64=disabled csmt=off grabfullscreen=y hosts nocrashdialog > /dev/null 2>&1
 tput sgr0
 
 cd ~/.PlayOnGit/setups/
-rm -f VC_redist.x64.exe
-rm -r VC_redist.x86.exe
-Get "https://download.visualstudio.microsoft.com/download/pr/3b070396-b7fb-4eee-aa8b-102a23c3e4f4/40EA2955391C9EAE3E35619C4C24B5AAF3D17AEAA6D09424EE9672AA9372AEED/VC_redist.x64.exe"
-Get "https://download.visualstudio.microsoft.com/download/pr/9307e627-aaac-42cb-a32a-a39e166ee8cb/E59AE3E886BD4571A811FE31A47959AE5C40D87C583F786816C60440252CD7EC/VC_redist.x86.exe"
-"$W"/bin/wine VC_redist.x64.exe /q
-"$W"/bin/wine VC_redist.x86.exe /q
+#rm -f VC_redist.x64.exe
+#rm -r VC_redist.x86.exe
+#Get "https://download.visualstudio.microsoft.com/download/pr/3b070396-b7fb-4eee-aa8b-102a23c3e4f4/40EA2955391C9EAE3E35619C4C24B5AAF3D17AEAA6D09424EE9672AA9372AEED/VC_redist.x64.exe"
+#Get "https://download.visualstudio.microsoft.com/download/pr/9307e627-aaac-42cb-a32a-a39e166ee8cb/E59AE3E886BD4571A811FE31A47959AE5C40D87C583F786816C60440252CD7EC/VC_redist.x86.exe"
+#"$W"/bin/wine VC_redist.x64.exe /q
+#"$W"/bin/wine VC_redist.x86.exe /q
 
 cd ~/.PlayOnGit/libraries/
-Get https://www.opencode.net/felipefacundes/wine-bins/raw/master/libraries/mfinstall.tar.xz
-tar -xf mfinstall.tar.xz
-cd mfinstall
-bash install-mf.sh > /dev/null 2>&1
+#Get https://www.opencode.net/felipefacundes/wine-bins/raw/master/libraries/mfinstall.tar.xz
+#tar -xf mfinstall.tar.xz
+#cd mfinstall
+#bash install-mf.sh > /dev/null 2>&1
 
 # DXVK - VULKAN
 cd ~/.PlayOnGit/libraries/dxvk/
@@ -233,7 +234,7 @@ echo "Progress ....."
 tput sgr0
 
 # Windows Version
-"$Wtricks" -q win10 csmt=off grabfullscreen=y > /dev/null 2>&1
+"$Wtricks" -q win7 csmt=off grabfullscreen=y > /dev/null 2>&1
 
 cd "$WINEPREFIX"
 Get https://raw.githubusercontent.com/felipefacundes/PS/master/Configs/EpicGamesStore/dxvk.conf
@@ -245,16 +246,16 @@ Get "https://github.com/felipefacundes/desktop/blob/master/explorerpp_1.3.5_x64/
 
 ######################### Setup executable/game here ##########################
 cd ~/.PlayOnGit/setups/
-rm -f EpicGamesLauncherInstaller.msi
-Get "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi" -O EpicGamesLauncherInstaller.msi
-"$W"/bin/msiexec /i EpicGamesLauncherInstaller.msi /q > /dev/null 2>&1
-Wkill
+#rm -f EpicGamesLauncherInstaller.msi
+#Get "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi" -O EpicGamesLauncherInstaller.msi
+#"$W"/bin/msiexec /i EpicGamesLauncherInstaller.msi /q > /dev/null 2>&1
+#Wkill
 
-sleep 3
-rm -f SteamSetup.exe
-Get https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe
-"$W"/bin/wine SteamSetup.exe
-Wkill
+#sleep 3
+#rm -f SteamSetup.exe
+#Get https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe
+#"$W"/bin/wine SteamSetup.exe
+#Wkill
 
 rm -f UbisoftConnectInstaller.exe
 Get https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UbisoftConnectInstaller.exe
