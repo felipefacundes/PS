6eb345626af562a0e68098f58b33a8377b31a069
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-22 20:11:47 -0300

    Update default-UbisoftConnect.sh

--- defaults/base/default-UbisoftConnect.sh
+++ defaults/base/default-UbisoftConnect.sh
@@ -175,6 +175,7 @@ echo "TUDO dependerá do seu PROCESSADOR. Abaixo de 3GHz demorará BEM mais."
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
 "$Wtricks" -q corefonts d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus > /dev/null 2>&1
+"$Wtricks" -q fontsmooth=rgb > /dev/null 2>&1
 
 echo
 echo
@@ -234,6 +235,7 @@ tput sgr0
 
 # Windows Version
 "$Wtricks" -q win10 csmt=off grabfullscreen=y > /dev/null 2>&1
+"$Wtricks" -q vkd3d > /dev/null 2>&1
 
 cd "$WINEPREFIX"
 Get https://raw.githubusercontent.com/felipefacundes/PS/master/Configs/EpicGamesStore/dxvk.conf
