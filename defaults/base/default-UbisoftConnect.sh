3500f905b8bd120f33ef5f0f9ec3f3ecfd88b6dc
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-22 20:12:22 -0300

    Update default-UbisoftConnect.sh

--- defaults/base/default-UbisoftConnect.sh
+++ defaults/base/default-UbisoftConnect.sh
@@ -228,6 +228,7 @@ cp -rf ~/.PlayOnGit/libraries/dxvk/dxvk-1.9.2/x64/* ~/.PlayOnGit/wineprefixes/"$
 cp -rf ~/.PlayOnGit/libraries/dxvk/dxvk-1.9.2/x32/* ~/.PlayOnGit/wineprefixes/"$GN"/drive_c/windows/syswow64/
 
 "$Wtricks" d3d9=native d3d10=native d3d10_1=native d3d10core=native d3d11=native dxgi=native > /dev/null 2>&1
+"$Wtricks" -q vkd3d > /dev/null 2>&1
 tput bold
 tput setaf 3
 echo "Progress ....."
@@ -235,7 +236,6 @@ tput sgr0
 
 # Windows Version
 "$Wtricks" -q win10 csmt=off grabfullscreen=y > /dev/null 2>&1
-"$Wtricks" -q vkd3d > /dev/null 2>&1
 
 cd "$WINEPREFIX"
 Get https://raw.githubusercontent.com/felipefacundes/PS/master/Configs/EpicGamesStore/dxvk.conf
