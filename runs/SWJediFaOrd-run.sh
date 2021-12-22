7f49af0a3de478f9b7e30c4b0dc07ca189489c92
Felipe Facundes <felipe.facundes@gmail.com>
2021-12-22 20:49:53 -0300

    Update SWJediFaOrd-run.sh

--- runs/SWJediFaOrd-run.sh
+++ runs/SWJediFaOrd-run.sh
@@ -14,7 +14,7 @@ Wkill
 rm -rf ~/.local/share/applications/*wine*
 clear -T "$TERM"
 
-export WV=wine-staging-7.0rc2.r0-x86_64
+export WV=wine-tkg-staging-6.22.r8-x86_64
 export GN="SWJediFaOrd"
 export SN="Star Wars Jedi Fallen Order"
 export CME="As one of the last Jedi, you must do whatever it takes to survive."
