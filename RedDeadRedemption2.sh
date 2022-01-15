46024406cf918af1799b7f5e9fcda9ef6ecad488
Felipe Facundes <felipe.facundes@gmail.com>
2022-01-14 21:36:18 -0300

    Update RedDeadRedemption2.sh

--- RedDeadRedemption2.sh
+++ RedDeadRedemption2.sh
@@ -174,7 +174,7 @@ echo "━━━━━━━━━━━━━━━━━━━━━━━━�
 echo "TUDO dependerá do seu PROCESSADOR. Abaixo de 3GHz demorará BEM mais."
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
-"$Wtricks" -q corefonts d3dx9 d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 > /dev/null 2>&1
+"$Wtricks" -q corefonts d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus > /dev/null 2>&1
 #"$W"/bin/wineboot > /dev/null 2>&1
 
 echo
