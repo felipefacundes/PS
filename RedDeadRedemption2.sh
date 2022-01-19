486ae233ec7ceeb5ab9e98fe8098cd7a43fc0a26
Felipe Facundes <felipe.facundes@gmail.com>
2022-01-18 21:21:08 -0300

    Update RedDeadRedemption2.sh

--- RedDeadRedemption2.sh
+++ RedDeadRedemption2.sh
@@ -19,7 +19,7 @@ rm -rf ~/.local/share/applications/*wine*
 whiptail --msgbox "Installation may take some time depending on the GAME. Above all, please: PATIENCE. WAIT! You will be notified when installation is complete." 10 30
 whiptail --msgbox "A instalação poderá demorar dependendo do JOGO. Acima de tudo tenha: PACIÊNCIA. AGUARDE! Você será notificado, quando a instalação concluir." 10 30
 
-export WV=wine-ge-custom-7.0rc6.GE.1-1-x86_64
+export WV=wine-ge-custom-7.0rc3.GE.1-1-x86_64
 export GN="RedDeadRedemption2"
 export SN="Red Dead Redemption 2"
 export CME="Red Dead Redemption 2 also includes the shared world of Red Dead online - trill your own path while facing authorities, out-of-law gangs and fierce wildlife to build a life at the United States border."
@@ -212,7 +212,7 @@ echo "Progress ..."
 "$Wtricks" -q vcrun2015 > /dev/null 2>&1
 #"$Wtricks" -q --force vcrun2017 > /dev/null 2>&1
 echo "Progress ...."
-"$Wtricks" autostart_winedbg=disabled nvapi=disabled nvapi64=disabled csmt=off grabfullscreen=y hosts nocrashdialog > /dev/null 2>&1
+"$Wtricks" autostart_winedbg=disabled nvapi=disabled nvapi64=disabled csmt=force grabfullscreen=y hosts nocrashdialog > /dev/null 2>&1
 tput sgr0
 
 cd ~/.PlayOnGit/setups/
@@ -244,7 +244,7 @@ echo "Progress ....."
 tput sgr0
 
 # Windows Version
-"$Wtricks" -q win7 csmt=off grabfullscreen=y > /dev/null 2>&1
+"$Wtricks" -q win7 csmt=force grabfullscreen=y > /dev/null 2>&1
 
 cd "$WINEPREFIX"
 Get https://raw.githubusercontent.com/felipefacundes/PS/master/Configs/EpicGamesStore/dxvk.conf
